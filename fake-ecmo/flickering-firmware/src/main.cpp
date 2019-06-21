231a093172e6c743af3c2e4214d1bebae230a29f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-06-21 22:00:25 +0200

    Implement new flickering algo using FPU

--- fake-ecmo/flickering-firmware/src/main.cpp
+++ fake-ecmo/flickering-firmware/src/main.cpp
@@ -5,12 +5,20 @@ uint8_t flickeringPin2 = 5;
 uint8_t flickeringPin3 = 6;
 uint8_t builtinLedPin = 13;
 
-unsigned int fps = 1000 / 75;
+double sinusMin = 0.6;
+double sinusMax = 1.0;
+double radiansPerSecond = 0.1;
+
+unsigned int fps = 15;
+
+double minFluorescent = 0.2;
+double minRandom = 0.85;
+double fluorescentFlickerPercent = 0.995;
+
+// static calc
 unsigned int delayMs = 1000 / fps;
-unsigned int minFluorescent = 30 * 100;
-unsigned int minRandom = 60 * 100;
-unsigned int max = 99 * 100;
-unsigned int fluorescentFlickerPercent = 985 * 10;
+double radiansPerFrame = radiansPerSecond / (double)fps;
+double fullCircleRadians = 2 * 3.141592;
 
 void setup()
 {
@@ -20,22 +28,28 @@ void setup()
   pinMode(builtinLedPin, OUTPUT);
 }
 
-unsigned int brightness = max;
+double brightness = 1.0;
 unsigned int brightnessRaw;
+double sinRotationRadians = 0.0;
 
 void flickerPin(uint8_t pin)
 {
-  brightness = max;
+  brightness = 1.0;
 
-  if (random(0, 10000) > fluorescentFlickerPercent)
+  sinRotationRadians += radiansPerFrame;
+  if (sinRotationRadians > fullCircleRadians)
+    sinRotationRadians -= fullCircleRadians;
+  brightness = sinusMin + sin(sinRotationRadians) * (sinusMax - sinusMin);
+
+  if (random(0, 10000) > (unsigned int)((double)10000 * fluorescentFlickerPercent))
   {
-    brightness = minFluorescent;
+    brightness *= minFluorescent;
   }
   else
   {
-    brightness = random(minRandom, max);
+    brightness *= ((double)random((long)(minRandom * 1000), 1000) / (double)1000);
   }
-  brightnessRaw = map(brightness, 0, 10000, 0, 255);
+  brightnessRaw = (unsigned int)(floor(brightness * (double)255));
   analogWrite(pin, brightnessRaw);
 }
 
