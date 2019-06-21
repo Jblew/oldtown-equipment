424b89fa4f7bb800b90a686d895a49edbd0b7c98
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-06-21 20:23:06 +0200

    Enhance flickering effect

--- fake-ecmo/flickering-firmware/src/main.cpp
+++ fake-ecmo/flickering-firmware/src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 
 uint8_t flickeringPin1 = 3;
-uint8_t flickeringPin2 = 4;
-uint8_t flickeringPin3 = 5;
+uint8_t flickeringPin2 = 5;
+uint8_t flickeringPin3 = 6;
+uint8_t builtinLedPin = 13;
 
 unsigned int fps = 1000 / 75;
 unsigned int delayMs = 1000 / fps;
-unsigned int minFluorescent = 50 * 100;
-unsigned int minRandom = 85 * 100;
+unsigned int minFluorescent = 30 * 100;
+unsigned int minRandom = 60 * 100;
 unsigned int max = 99 * 100;
 unsigned int fluorescentFlickerPercent = 985 * 10;
 
@@ -16,6 +17,7 @@ void setup()
   pinMode(flickeringPin1, OUTPUT);
   pinMode(flickeringPin2, OUTPUT);
   pinMode(flickeringPin3, OUTPUT);
+  pinMode(builtinLedPin, OUTPUT);
 }
 
 unsigned int brightness = max;
@@ -42,5 +44,6 @@ void loop()
   flickerPin(flickeringPin1);
   flickerPin(flickeringPin2);
   flickerPin(flickeringPin3);
+  flickerPin(builtinLedPin);
   delay(delayMs);
 }
